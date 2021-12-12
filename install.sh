c14d24d6bde219b53841450143963aa2988f1ce4
FleetAdmiralButter <ming.quah@protonmail.com>
2021-12-12 20:17:37 +1100

    Clean up

--- install.sh
+++ install.sh
@@ -1,24 +1,47 @@
-#!/bin/bash
+#!/bin/sh
 
-# Bootstrap the Wine environment
-wrapperWine="$PWD/D3.app/Contents/SharedSupport/wine/bin"
-export PATH="$PATH:$wrapperWine"
-echo "Using $wrapperWine as PATH"
+WINESKIN_TARGET_NAME="template_xiv_working.app"
 
-winetricks="$PWD/D3.app/Wineskin.app/Contents/Resources/winetricks"
-echo "Using winetricks at $winetricks"
+wrapperWine=""
+winetricks=""
+prefix=""
 
-prefix="$PWD/D3.app/Contents/Resources"
-echo "Using prefix $prefix"
-export WINEPREFIX=$prefix
+function boot() {
+	wrapperWine="$PWD/template_xiv_working.app/Contents/SharedSupport/wine/bin"
+	export PATH="$PATH:$wrapperWine"
 
-$winetricks list-installed
+	winetricks="$PWD/template_xiv_working.app/Wineskin.app/Contents/Resources/winetricks"
 
-echo "Installing dependencies..."
+	prefix="$PWD/template_xiv_working.app/Contents/Resources"
+	export WINEPREFIX=$prefix
+}
 
-$winetricks dotnet40
-$winetricks dotnet40_kb2468871
-$winetricks vcrun2015
-$winetricks vcrun2012
-$winetricks vcrun2010
-$winetricks corefonts
+function install_deps() {
+	$winetricks dotnet40
+	$winetricks dotnet40_kb2468871
+	$winetricks dotnet472
+	$winetricks vcrun2015
+	$winetricks vcrun2012
+	$winetricks vcrun2010
+	#$winetricks corefonts
+}
+
+echo "==> Removing Gatekeeper quarantine from downloaded wrapper. You may need to enter your password."
+sudo xattr -r -d com.apple.quarantine "$PWD/$WINESKIN_TARGET_NAME" &>/dev/null
+
+echo "==> Start bootstrap of the Wine environment."
+boot
+echo "==> Try to validate the bootstrap."
+$winetricks list-installed &>/dev/null
+isWorkingEnv=$?
+
+if [ "$isWorkingEnv" != "0" ]; then
+	echo "==> Could not validate that the Wine environment is working correctly, sorry."
+	exit 1;
+fi
+
+echo "==> Finished bootstrapping the Wine environment."
+
+echo "==> Installing proprietary dependencies..."
+install_deps
+echo "==> Finished installing dependencies."
